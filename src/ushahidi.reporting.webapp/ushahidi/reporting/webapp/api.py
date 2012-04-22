82d9def6862d3f6d2857f91db6bdfd8a94cd8201
Riccardo Lemmi <riccardo@reflab.com>
2012-04-22 16:56:23 +0200

    fixed fake 'task' service

--- src/ushahidi.reporting.webapp/ushahidi/reporting/webapp/api.py
+++ src/ushahidi.reporting.webapp/ushahidi/reporting/webapp/api.py
@@ -7,6 +7,41 @@ import time
 from pprint import pprint
 from paste.request import parse_formvars
 
+CATEGORIES = { 
+    "payload": {
+        "domain": None,   # to set
+        "categories":[
+            {"category":
+                { "id":"1",
+                  "parent_id":"0",
+                  "title":"Category 1",
+                  "description":"Category 1",
+                  "color":"9900CC","position":"0"}},
+            {"category":
+                { "id":"2",
+                  "parent_id":"0",
+                  "title":"Category 2",
+                  "description":"Category 2",
+                  "color":"3300FF",
+                  "position":"0"}},
+            {"category":
+                { "id":"3",
+                  "parent_id":"0",
+                  "title":"Category 3",
+                  "description":"Category 3",
+                  "color":"663300",
+                  "position":"0"}},
+            {"category":
+                { "id":"4",
+                  "parent_id":"0",
+                  "title":"Trusted Reports",
+                  "description":"Reports from trusted reporters",
+                  "color":"339900","position":"0"}}
+        ]},
+    "error":{"code":"0","message":"No Error"}
+}
+  
+                    
 class FakeAPI(object):
     """ to test the request submitted """
 
@@ -22,48 +57,23 @@ class FakeAPI(object):
         #pprint(environ)
         #import pdb; pdb.set_trace()
 
+        CATEGORIES['payload']['domain']= environ.get('HTTP_ORIGIN')
+        
         if environ.get('REQUEST_METHOD','') == 'POST':
-            result = { "payload": {
-                          "domain": environ.get('HTTP_ORIGIN'),
-                          "success":"true"
-                        },
-                        "error":{ "code":"0",
-                                  "message":"No Error"}
-                    }
+            #~ import pdb; pdb.set_trace()
+            if  environ['paste.parsed_formvars'][0].has_key('task'):
+                result = CATEGORIES  
+            else:
+                result = { "payload": {
+                              "domain": environ.get('HTTP_ORIGIN'),
+                              "success":"true"
+                            },
+                            "error":{ "code":"0",
+                                      "message":"No Error"}
+                        }
         elif environ.get('REQUEST_METHOD','') == 'GET' and \
              environ.get('QUERY_STRING', '') == 'task=categories':
-            result = { "payload": {
-                          "domain": environ.get('HTTP_ORIGIN'),
-                          "categories":[
-                              {"category":
-                                  { "id":"1",
-                                    "parent_id":"0",
-                                    "title":"Category 1",
-                                    "description":"Category 1",
-                                    "color":"9900CC","position":"0"}},
-                              {"category":
-                                  { "id":"2",
-                                    "parent_id":"0",
-                                    "title":"Category 2",
-                                    "description":"Category 2",
-                                    "color":"3300FF",
-                                    "position":"0"}},
-                              {"category":
-                                  { "id":"3",
-                                    "parent_id":"0",
-                                    "title":"Category 3",
-                                    "description":"Category 3",
-                                    "color":"663300",
-                                    "position":"0"}},
-                              {"category":
-                                  { "id":"4",
-                                    "parent_id":"0",
-                                    "title":"Trusted Reports",
-                                    "description":"Reports from trusted reporters",
-                                    "color":"339900","position":"0"}}
-                          ]},
-                        "error":{"code":"0","message":"No Error"}
-                    }
+            result = CATEGORIES
         else:
             result = {"status": "ok", "now": int(time.time())}
 
